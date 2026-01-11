5207cee22b4fada0d06d6c03a2f0feb56ce1a81f
navijose24 <navaneethapjose@gmail.com>
2026-01-11 13:18:52 +0530

    variable

---
+++ variables/main.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+func main() {
+	var message string = "Hello, Go Variables!"
+	leters:=16 //short variable deleration
+	fmt.Println(message)
+	fmt.Println("Number of letters:", leters)
+}
